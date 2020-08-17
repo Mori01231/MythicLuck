c104103dd2ad87358f025908f7736cc3380c5047
Mori01231 <mori01231mc@gmail.com>
2020-08-18 01:19:03 +0900

    [Add] Initial commit.

---
+++ src/main/java/com/github/mori01231/mythicluck/MythicLuck.java
@@ -0,0 +1,17 @@
+package com.github.mori01231.mythicluck;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MythicLuck extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
