02e627707db1306c89da80625545d6a14362e605
Mori01231 <mori01231mc@gmail.com>
2020-08-18 03:19:10 +0900

    [Add] Declaring all of the variables.

--- src/main/java/com/github/mori01231/mythicluck/GiveCommandExecutor.java
+++ src/main/java/com/github/mori01231/mythicluck/GiveCommandExecutor.java
@@ -1,5 +1,6 @@
 package com.github.mori01231.mythicluck;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,6 +10,33 @@ import static org.bukkit.Bukkit.getServer;
 public class GiveCommandExecutor implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        String playerName = args[0];
+        String mmItemName = args[1];
+
+        try{
+            Integer mmItemNumber = Integer.valueOf(args[2]);
+            if(mmItemNumber < 0){
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"渡す個数は正の整数で指定してください。"));
+                return true;
+            }
+        }catch(Exception e){
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"渡す個数は正の整数で指定してください。"));
+            return true;
+        }
+
+        try{
+            Double mmItemChance = Double.valueOf(args[2]);
+            if (mmItemChance < 0){
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"渡す個数は正の整数または少数で指定してください。"));
+                return true;
+            }
+        }catch(Exception e){
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"渡す個数は正の整数または少数で指定してください。"));
+            return true;
+        }
+
+
         return false;
     }
 
