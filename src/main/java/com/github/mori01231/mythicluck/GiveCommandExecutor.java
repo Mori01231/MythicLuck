7afd785514aafadfbc406f6da6b3deb6e951c520
Mori01231 <mori01231mc@gmail.com>
2020-08-18 02:06:05 +0900

    [Add] sendCommand method to simplify sending commands.

--- src/main/java/com/github/mori01231/mythicluck/GiveCommandExecutor.java
+++ src/main/java/com/github/mori01231/mythicluck/GiveCommandExecutor.java
@@ -4,9 +4,15 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import static org.bukkit.Bukkit.getServer;
+
 public class GiveCommandExecutor implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         return false;
+    }
+
+    public void sendCommand(String command){
+        getServer().dispatchCommand(getServer().getConsoleSender(), command);
     }
 }
