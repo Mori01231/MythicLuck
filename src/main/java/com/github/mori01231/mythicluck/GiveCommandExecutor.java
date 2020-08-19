f62f0bd1555204c5749ca4dde9b845f631ac958a
Mori01231 <mori01231mc@gmail.com>
2020-08-19 22:03:52 +0900

    [Add] Detect luck attribute value for player.

--- src/main/java/com/github/mori01231/mythicluck/GiveCommandExecutor.java
+++ src/main/java/com/github/mori01231/mythicluck/GiveCommandExecutor.java
@@ -1,10 +1,14 @@
 package com.github.mori01231.mythicluck;
 
 import org.bukkit.ChatColor;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
+import static org.bukkit.Bukkit.getPlayer;
 import static org.bukkit.Bukkit.getServer;
 
 public class GiveCommandExecutor implements CommandExecutor {
@@ -15,7 +19,9 @@ public class GiveCommandExecutor implements CommandExecutor {
         String mmItemName = args[1];
         Integer giveNumber = 0;
         Double luckNumber = 0.0;
+        Player player = getPlayer(playerName);
 
+        luckNumber = player.getAttribute(Attribute.GENERIC_LUCK).getValue();
 
         try{
             Integer mmItemNumber = Integer.valueOf(args[2]);
